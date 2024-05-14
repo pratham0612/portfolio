a11b987d12e14f803f81835c097cb162b23414a2
Pratham Panchal <102433807+pratham0612@users.noreply.github.com>
2024-05-14 18:31:09 -0400

    Update Blog.jsx

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -11,8 +11,8 @@ const Blog = () => {
             <div className="blog__container grid">
                 <div className="blog__card">
                     <div className="blog__thumb">
-                        <a href="#"><span className="blog__category">Example</span></a>
-                        <a href="#"><img src={Image1} alt="" className='blog__img' /></a>
+                       {/* <a href="#"><span className="blog__category">Example</span></a> */}
+                       {/* <a href="#"><img src={Image1} alt="" className='blog__img' /></a> */}
                     </div>
                     <div className="blog__details">
                         <h3 className="blog__title">Example Blog</h3>
@@ -28,4 +28,4 @@ const Blog = () => {
     )
 }
 
-export default Blog
+export default Blog;
